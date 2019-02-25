6edc1e72391ded2b240da9352d7b7cc6e00b98f6
jasilven <jasilven@gmail.com>
2019-02-25 13:09:32 +0200

    added leiningen support

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject aoc-2018 "1.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/data.priority-map "0.0.10"]
+                 [quil "2.8.0" :exclusions [com.lowagie/itext]]])
